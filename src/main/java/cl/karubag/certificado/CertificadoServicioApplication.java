f2bedeb46aede5347c9c0e702bb7f3876d37a7cb
klaw3134 <cgnavarrete3134@gmail.com>
2026-05-28 18:44:21 -0400

    certificado-servicio

---
+++ src/main/java/cl/karubag/certificado/CertificadoServicioApplication.java
@@ -0,0 +1,13 @@
+package cl.karubag.certificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificadoServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificadoServicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/karubag/certificado/CertificadoServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.karubag.certificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificadoServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
